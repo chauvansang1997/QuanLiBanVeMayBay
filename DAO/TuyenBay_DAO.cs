088375fb83bade9d2542c625978b8474d842f609
SANG CHÂU VĂN <15520717@ms.uit.edu.vn>
2017-06-07 13:05:08 +0700

    thay doi

--- DAO/TuyenBay_DAO.cs
+++ DAO/TuyenBay_DAO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -9,15 +10,59 @@ namespace DAO
 {
     public static class TuyenBay_DAO
     {
-        public static int DemTuyenBay(string _sanBayDi,string _sanBayDen)
+        public static int DemTuyenBay(string _sanBayDi,string _sanBayDen,int pageSize,int pageNumber)
         {
-            string query = "EXEC usp_DemTuyenBay @sanBayDi,@sanBayDen";
+            string query = "EXEC usp_DemTuyenBay @sanBayDi,@sanBayDen,@pageSize,@pageNumber";
 
             List<SqlParameter> parameters = new List<SqlParameter>()
             {
-                new SqlParameter("@sanBayDi")
+                new SqlParameter("@sanBayDi",SqlDbType.VarChar){IsNullable=true,Value = _sanBayDi??(Object)DBNull.Value},
+
+                new SqlParameter("@sanBayDen",SqlDbType.VarChar){IsNullable=true,Value = _sanBayDen??(Object)DBNull.Value},
+
+                new SqlParameter("@pageSize",SqlDbType.Int){Value = _sanBayDen },
+
+                new SqlParameter("@pageNumber",SqlDbType.Int){Value = _sanBayDen },
             };
 
+            try
+            {
+                return (int)Dataprovider.ExcuteScalar(query, parameters.ToArray());
+            }
+            catch (Exception err)
+            {
+
+                Help_Fuction.HelpFuction.Log(err);
+            }
+            return 0;
         }
+        public static DataTable TimTuyenBay(string _sanBayDi, string _sanBayDen, int pageSize, int pageNumber)
+        {
+            string query = "EXEC usp_TimTuyenBay @sanBayDi,@sanBayDen,@pageSize,@pageNumber";
+
+            List<SqlParameter> parameters = new List<SqlParameter>()
+            {
+                new SqlParameter("@sanBayDi",SqlDbType.VarChar){IsNullable=true,Value = _sanBayDi??(Object)DBNull.Value},
+
+                new SqlParameter("@sanBayDen",SqlDbType.VarChar){IsNullable=true,Value = _sanBayDen??(Object)DBNull.Value},
+
+                new SqlParameter("@pageSize",SqlDbType.Int){Value = _sanBayDen },
+
+                new SqlParameter("@pageNumber",SqlDbType.Int){Value = _sanBayDen },
+            };
+
+            try
+            {
+                return Dataprovider.ExcuteQuery(query, parameters.ToArray());
+            }
+            catch (Exception err)
+            {
+
+                Help_Fuction.HelpFuction.Log(err);
+            }
+
+            return null;
+        }
+        public 
     }
 }
