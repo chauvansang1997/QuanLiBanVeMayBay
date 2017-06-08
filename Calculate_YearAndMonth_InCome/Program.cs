da7eb6814b98d60d91fc83a559f8e09e72ae2572
SANG CHÂU VĂN <15520717@ms.uit.edu.vn>
2017-06-08 23:46:12 +0700

    thAY DOI

--- Calculate_YearAndMonth_InCome/Program.cs
+++ Calculate_YearAndMonth_InCome/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,9 +12,20 @@ namespace Calculate_YearAndMonth_InCome
 {
     class Program
     {
+        
+        [DllImport("Kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd,int  cmdShow);
+
         static void Main(string[] args)
         {
-            
+            RegisTry_Help REG = new RegisTry_Help();
+            IntPtr hWnd = GetConsoleWindow();
+            if (hWnd != IntPtr.Zero)
+            {
+                ShowWindow(hWnd, 0);
+            }
             while (true)
             {
                 //Mỗi 1 phút kiểm tra xem có tới tháng tiếp theo chưa
---
+++ Calculate_YearAndMonth_InCome/RegisTry_Help.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Calculate_YearAndMonth_InCome
+{
+    class RegisTry_Help
+    {
+        public RegistryKey reg;
+        public RegisTry_Help()
+        {
+            reg = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            reg.SetValue("Tinh Doanh Thu", System.Reflection.Assembly.GetEntryAssembly().Location.ToString());
+        }
+    }
+}
